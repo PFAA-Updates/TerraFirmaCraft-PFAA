f09d0eac3bf0d8d78c57220936a4ac4dbf0c7807
quentin452 <42176772+quentin452@users.noreply.github.com>
2024-06-02 19:00:01 +0200

    Fix buildscript + workspace + fix again a dep + Fix compilation error
    note : this version break probably addons because i needed to change the modGroup

---
+++ src/main/java/com/bioxx/tfcnew/Blocks/Liquids/BlockLava.java
@@ -0,0 +1,12 @@
+package com.bioxx.tfc.Blocks.Liquids;
+
+import net.minecraft.block.material.Material;
+
+import com.bioxx.tfc.api.TFCFluids;
+
+public class BlockLava extends BlockCustomLiquid {
+
+    public BlockLava() {
+        super(TFCFluids.LAVA, Material.lava);
+    }
+}
